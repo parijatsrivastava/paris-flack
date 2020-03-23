e59cbee6f819240527035bf1f0e5b5c6cd2b2c7f
parijatsrivastava <parijat.srivastva@outlook.com>
2020-03-23 23:07:43 +0530

    ORM changes

--- models.py
+++ models.py
@@ -12,6 +12,7 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     username = db.Column(db.String, nullable=False, unique=True)
     channels = db.relationship("Channel", backref="user", lazy=True)
+    chats = db.relationship("Chat", backref="user", lazy=True)
 
 class Channel(db.Model):
     __tablename__ = "channels"
@@ -24,6 +25,6 @@ class Chat(db.Model):
     __tablename__ = "chats"
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String, nullable=False)
-    username = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, db.ForeignKey("users.username"), nullable=False)
     time = db.Column(db.DateTime, nullable=False)
     channelname = db.Column(db.String, db.ForeignKey("channels.name"), nullable=False)
